8b251a0694367b23847ca7b5c38c2735f885a4a7
Alexander Ma <Thatguyalex@gmail.com>
2017-05-20 21:18:18 -0400

    testing

--- gitpush.bat
+++ gitpush.bat
@@ -1,8 +1,8 @@
 ECHO Adding, Committing, and Pushing Projector to Github
 set /p comMsg=Commit Message: 
 
-git eval $(ssh-agent -s)
-git ssh-add /c/Users/Alexander/.ssh/github_rsa
+eval $(ssh-agent -s)
+ssh-add /c/Users/Alexander/.ssh/github_rsa
 git add *
 git commit -m "%comMsg%"
 git push -u mittens master
